851886311b51605f5452cebdf9b570df940eec69
Dave Sheremata <dsheremata@t-sciences.com>
2018-01-11 10:25:46 -0500

    Updated vnc-docker.sh to launch browser automagically.

--- vnc-docker.sh
+++ vnc-docker.sh
@@ -1,7 +1,8 @@
 eval export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://172.16.82.130:2376"
+export DOCKER_MACHINE_IP=`docker-machine ip`
+export DOCKER_HOST="tcp://"${DOCKER_MACHINE_IP}":2376"
 export DOCKER_CERT_PATH="/Users/dsheremata/.docker/machine/machines/default"
 export DOCKER_MACHINE_NAME="default"
 # Run this command to configure your shell: 
 # eval $(docker-machine env default)
-open http://172.16.82.130:8080/vnc.html
+open http://${DOCKER_MACHINE_IP}:6901/?password=vncpassword/
